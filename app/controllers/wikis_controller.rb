0565bc86761fa0ef0214c9c5ffe4327f973eea78
technologyRiff <technologyriff@gmail.com>
2015-01-31 14:40:32 -0500

    created new and edit views

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -8,8 +8,37 @@ class WikisController < ApplicationController
   end
 
   def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new()
+    if @wiki.save
+      flash[:notice] = "Wiki was saved."
+      redirect_to @wiki
+    else 
+      flash[:error] = "There was an error saving the post. Please try again."
+    end
   end
 
   def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    if @wiki.update_attributes
+      flash[:notice] = "Wiki was updated."
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error saving the wiki. Please try again."
+      render :edit
+    end
+  end
+
+  private
+
+  def params_wiki
+    params.require(:wiki).permit(:title, :body)
   end
 end
