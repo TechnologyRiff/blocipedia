cb3cfa37e44d862eee45c6fdaf5e9f5b94518ec9
technologyRiff <technologyriff@gmail.com>
2015-03-01 21:15:05 -0500

    fixed stripe on homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,11 @@ class WelcomeController < ApplicationController
   def index
     @wikis = Wiki.all
     #authorize @wiki
+    @stripe_btn_data = {
+      key: Rails.configuration.stripe[:publishable_key],
+      description: "Premium Membership - #{current_user.name}",
+      amount: Amount.default
+      }
   end
 
   def about
